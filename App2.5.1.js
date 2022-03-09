537005a6ad522c2cbd51d574e5817e392bf510c5
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2022-03-09 17:39:27 +0900

    Create App2.5.1.js

---
+++ App2.5.1.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  const name = "리액트";
+  return <div className="react">{name}</div>;   //JSX에서는 어떤 태그든 무조건 닫아야함(ex. input, br)
+  {/* 주석은 이렇게 작성합니다 */}
+  {/* ESLint --> 문법 검사 도구 */}
+  {/* Prettier --> 코드 스타일 자동 정리*/}
+  
+}
+
+
+
+
+export default App;
