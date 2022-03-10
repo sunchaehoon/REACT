effd60645299403b724a3d122dd77b0928e749a5
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2022-03-10 10:13:40 +0900

    Create App3.3.4.js

---
+++ App3.3.4.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import "./App.css";
+import MyComponent from './MyComponent';
+
+
+const App = () => {
+  return <MyComponent>리액트</MyComponent>;   // 컴포넌트 태그 사이의 내용을 보여 주는 children
+};
+
+export default App;
