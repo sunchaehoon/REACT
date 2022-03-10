1e8b2bf3f966478790beaa5e56a0455f09242a7a
sunchaehoon <sunchaehoon@naver.com>
2022-03-10 19:44:21 +0900

    test commit

---
+++ src/Counter.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+
+class Counter extends Component {
+    // state의 초깃값 설정하기
+    state = {
+        number: 0,
+        fixedNumber: 0
+    };
+
+    render() {
+        const {number, fixedNumber} = this.state;    // state를 조회할 때는 this.state로 조회한다
+        return (
+            <div>
+                <h1>{number}</h1>
+                <h2>바뀌지 않는 값: {fixedNumber}</h2>
+                <button
+                // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정한다
+                onClick={() => {
+                    // this.setState를 사용하여 state에 새로운 값을 넣을 수 있다
+                    this.setState(
+                        {
+                            number: number + 1
+                        },
+                        () => {
+                            console.log('setState가 호출됨');
+                            console.log(this.state);
+                        }
+                    );
+                }}
+            >
+                +1
+                </button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
