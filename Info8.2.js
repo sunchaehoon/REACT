0c496e499cc75be304229451ef0a2f552b896f16
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2022-03-14 16:37:01 +0900

    Create Info8.2.js

---
+++ Info8.2.js
@@ -0,0 +1,43 @@
+import React, {useState, useEffect} from 'react';
+// useEffect() --> 리액트 컴포넌트가 렌더링될 때마다 특정 작업을 수행하도록 설정할 수 있는 Hook (componentDidMount + componentDidUpdate)
+// componentDidMount() --> 컴포넌트가 마운트된 직후, 즉 트리에 삽입된 직후에 호출됨
+// componentDIdUpdate() --> 
+
+const Info = () => {
+    const [name, setName] = useState('');
+    const [nickname, setNickname] = useState('');
+    useEffect(() => {
+        console.log('렌더링이 완료되었습니다');
+        console.log({
+            name,
+            nickname
+        });
+    });
+
+    const onChangeName = e => {
+        setName(e.target.value);
+    };
+
+    const onChangeNickname = e => {
+        setNickname(e.target.value);
+    };
+
+    return (
+        <div>
+            <div>
+                <input value={name} onChange={onChangeName} />
+                <input value={nickname} onChange={onChangeNickname} />
+            </div>
+            <div>
+                <div>
+                    <b>이름:</b> {name}
+                </div>
+                <div>
+                    <b>닉네임:</b> {nickname}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Info;
